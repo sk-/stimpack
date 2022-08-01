76715f7903e76f61a57b90d4b7670d1ed02b6dd4
Sebastian Kreft <sk-@users.noreply.github.com>
2022-08-01 18:08:22 -0400

    feat: recursive root
    Check for paths recursively within the root. This would allow to better organize large folders.
    
    One limitation of this PR is that it would only allow one root, which could certainly be a limitation.

--- lib/stimpack/packs.rb
+++ lib/stimpack/packs.rb
@@ -13,8 +13,8 @@ module Stimpack
 
       def resolve
         # Gather all the packs under the root directory and create packs.
-        root.children.select(&:directory?).sort!.each do |path|
-          next unless pack = Pack.create(path)
+        root.glob(File.join("**", "package.yml")).sort!.each do |path|
+          next unless pack = Pack.create(path.dirname)
           @packs[pack.name] = pack
         end
         @packs.freeze
